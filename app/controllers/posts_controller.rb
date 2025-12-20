940aad8cf2bf250f8a516c7d90dc880adc4bbaf7
mt-westfall <234342056+mt-westfall@users.noreply.github.com>
2025-12-20 13:52:10 +0900

    [update]投稿一覧機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,16 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new(post_params)
+    post.save
+    redirect_to '/top'
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:location, :text)
   end
 end
