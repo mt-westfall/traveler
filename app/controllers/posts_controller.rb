f5297ca02dcde0b327f4b1725e0446b4aacf6bed
mt-westfall <234342056+mt-westfall@users.noreply.github.com>
2025-12-21 17:36:20 +0900

    [update]編集ページを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,25 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.save
-    redirect_to '/top'
+    redirect_to post_path(post.id)
   end
 
   def index
     @posts = Post.all
+  end
+
+  def show 
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   private
